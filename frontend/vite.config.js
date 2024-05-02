51680e94c0a3f66741c89e6a3b8e3887af60c5de
imrankhan919 <imrankhan919.ik@gmail.com>
2024-05-02 12:41:43 +0530

    server_config_updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mern-crud-h6t8.onrender.com/",
-        changeOrigin: true,
-        isSecure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://mern-crud-h6t8.onrender.com/",
+  //       changeOrigin: true,
+  //       isSecure: false,
+  //     },
+  //   },
+  // },
 });
