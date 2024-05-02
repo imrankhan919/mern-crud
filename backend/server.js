8b8527ff8b3da3000208f43b18238ce4e6e7dee4
imrankhan919 <imrankhan919.ik@gmail.com>
2024-05-02 12:36:44 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
